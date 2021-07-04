5c61c78f73416eea8cd0498cd6eb7ffcbea45c30
ateb14 <85857627+ateb14@users.noreply.github.com>
2021-07-05 00:32:39 +0800

    Update Buffered Channels and Worker Pools.go

--- Buffered Channels and Worker Pools.go
+++ Buffered Channels and Worker Pools.go
@@ -7,40 +7,39 @@ import (
 )
 
 type Task struct{
-	id,mission int
+	id int
 }
 type Result struct{
 	task Task
-	ret int
 }//completed tasks
 
 var tasks = make(chan Task,10)
 var results = make(chan Result,10)
 
-func DoingTheTask(mission int) int{
+func DoingTheTask() {
 	time.Sleep(time.Second)//simulation of the time that is needed to finish the task
-	return mission
 }
 func distribute(NumberOfTask int){
 	for i:=0;i<NumberOfTask;i++{
 		id := i
-		mission := 2*i
-		task := Task{id, mission}
+		task := Task{id}
 		tasks <- task//distributing the tasks to the channel
 		fmt.Println("Task ",id," is distributed")
 	}
 	close(tasks)
 }
+
 func PrintResult(flag chan bool){
 	for result := range results{
-		fmt.Println("Task ",result.task.id," is finished,and the result of it is ",result.ret)
+		fmt.Println("Task ",result.task.id," is finished")
 	}
 	flag<-true
 }
 
 func worker(wg *sync.WaitGroup){
 	for task := range tasks{
-		ret := Result{task, DoingTheTask(task.mission)}
+		DoingTheTask()
+		ret := Result{task}
 		results <- ret
 	}
 	wg.Done()
